363bc1b0c93dabb1db405ee3fa42dd0c055af7d7
Jeremy O'Daniel <jeremy.odaniel@gmail.com>
2022-01-22 17:51:27 -0600

    Create questions array for user input.

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,34 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please describe your project.'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your Github Username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
