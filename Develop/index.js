ac04df1e090bcb60577013cce8c4efb400e44b81
Jeremy O'Daniel <jeremy.odaniel@gmail.com>
2022-01-23 18:52:38 -0600

    Complete questions section of index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,34 +1,59 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
 const fs = require('fs');
+const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
 
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {
+      {
         type: 'input',
         name: 'title',
-        message: 'What is your project title?',
-    },
-    {
+        message: 'What is your project title? ',
+      },
+      {
         type: 'input',
         name: 'description',
-        message: 'Please describe your project.'
-    },
-    {
+        message: 'Please describe your project: ',
+      },
+			{
+				type: 'input',
+				name: 'usage',
+				message: 'Please describe the usage of this project: ',
+			},
+      {
         type: 'input',
         name: 'github',
-        message: 'What is your Github Username?',
-    },
-    {
+        message: 'What is your Github Username? ',
+      },
+      {
         type: 'input',
         name: 'email',
-        message: 'What is your email?',
-    },
-];
+        message: 'What is your email? ',
+      },
+			{
+				type: 'list',
+				name: 'license',
+				message: 'Choose a license for your project.',
+				choices: ['MIT', 'Apache 2.0', 'BSD', 'GPL', 'MPL',]
+			},
+			{
+				type: 'input',
+				name: 'installation',
+				message: 'What steps are requird to install this project? ',
+			},
+			{
+				type: 'input',
+				name: 'contributions',
+				message: 'How can others contribute to this project? ',
+			},
+];			
+
+
 console.log(questions);
+
+
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
